d53928f107103f6f731b624ff1528a8fab904837
MattBanuag <matthewbanuag.dev@gmail.com>
2022-12-12 13:08:06 -0600

    map added

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,12 +1,37 @@
 'use strict';
 
 // BRIDGE TO MAP CONTAINER
-const mapContainer = document.querySelector('#map');
+const marker = new mapboxgl.Marker();
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWF0dGJhbnVhZyIsImEiOiJjbGJncnE5bXUwNHVsM3Budzg5NXluNzJjIn0.HXWxK3IT6MmGfuZb1_sSbw';
 const map = new mapboxgl.Map({
-    container: mapContainer, // container ID
+    container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9, // starting zoom
+    center: [0, 0], // starting position [lng, lat]
+    zoom: 13, // starting zoom
 });
+
+// MARKER
+
+// FUNCTIONS
+function getLocation(position) {
+    const { longitude, latitude } = position.coords;
+
+    // Create a new marker
+    map.setCenter([longitude, latitude]);
+    marker.setLngLat([longitude, latitude]).addTo(map);
+}
+
+function errorHandler(error) {
+    console.log(error.message);
+}
+
+const options = {
+    enableHighAccuracy: true
+};
+
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(getLocation, errorHandler);
+} else {
+    console.log('Geolocation is not supported by your browser');
+}
