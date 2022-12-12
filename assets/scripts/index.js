58e9aadfc5ce0294cbb3e058ad11eed74f5eacdd
MattBanuag <matthewbanuag.dev@gmail.com>
2022-12-12 10:28:50 -0600

    add map and styling

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,2 +1,12 @@
 'use strict';
-import { select, onEvent, create } from './utils.js';
+
+// BRIDGE TO MAP CONTAINER
+const mapContainer = document.querySelector('#map');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWF0dGJhbnVhZyIsImEiOiJjbGJncnE5bXUwNHVsM3Budzg5NXluNzJjIn0.HXWxK3IT6MmGfuZb1_sSbw';
+const map = new mapboxgl.Map({
+    container: mapContainer, // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9, // starting zoom
+});
